d0ce73f0cba8f8f72230d2cbfe30c6e4b7bfe45d
McLeanBH <mcleanbh@gmail.com>
2015-02-05 20:56:59 -0500

    workn on some js

--- scripts/main.js
+++ scripts/main.js
@@ -15,4 +15,22 @@
     });
 
 
+    function activePlayerTurn() {
+      console.log("It's someone's turn");
+    }
+
+    function gamePlay(countDonuts, countCops) {
+      var charactersCount = countDonuts + countCops;
+      var activeCharacters;
+    }
+
+    function selectActivePlayer(autos, humans) {
+      var a = autos;
+      var h = humans;
+      // if (h > 0) {
+      //     startShowingIt(.)
+      // }
+
+    }
+
 })();
