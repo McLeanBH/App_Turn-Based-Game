935f0aecffc9ec273fb95a18e8ad2d2a569be945
Taylor Aull <aull.taylor@gmail.com>
2015-02-05 20:33:40 -0500

    More Changes

--- scripts/main.js
+++ scripts/main.js
@@ -2,21 +2,17 @@
   'use strict';
 
 
-    $('.select-character').on('submit', function(){
+    $('#donut-button').on('click', function(){
       event.preventDefault();
-
-      if($(".username-input").val() === '') {
-        alert("Please enter an username");
-      } else {
-        usernameInput = $(".username-input").val();
-        console.log(usernameInput);
-        $('.username-filler').text(usernameInput);
-        $('.msg-container').removeClass('hidden');
-        $('.msg-container').scrollTop($('.msg-container')[0].scrollHeight);
-        $('.msg-input-container').removeClass('hidden');
-        $('.login-container').addClass('hidden');
-        $('.welcome').addClass('hidden');
-      }
+        $('.game-container').removeClass('hidden');
+        $('.container-login-page').addClass('hidden');
     });
+
+    $('#cop-button').on('click', function(){
+      event.preventDefault();
+      $('.game-container').removeClass('hidden');
+      $('.container-login-page').addClass('hidden');
+    });
+
 
 })();
